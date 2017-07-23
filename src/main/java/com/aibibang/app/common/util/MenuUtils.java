fecc00bf7cafce7e96bd57949a5b11eb76851b0a
truman <1753177225@qq.com>
2017-07-23 21:56:06 +0800

    update resources

--- src/main/java/com/aibibang/app/common/util/MenuUtils.java
+++ src/main/java/com/aibibang/app/common/util/MenuUtils.java
@@ -7,72 +7,72 @@ import java.util.List;
 
 public class MenuUtils {
 
-	
-	public static String getMenu(List<SysMenu> menuList, List<Long> userMenus){
-		
-		StringBuilder sb = new StringBuilder();
-		
-		if(menuList != null && menuList.size() > 0){
-			
-			for(SysMenu menu : menuList){
-				
-				if(userMenus.contains(menu.getId())){
-					int childNum = menu.getChildren().size();
-					
-					sb.append("<li>\n")
-					.append("<a href=\"javascript:;\" ");
-					if(StringUtils.isNotEmpty(menu.getMenuUrl()) && childNum == 0){
-						sb.append("onclick=\"menu(this,'"+menu.getMenuUrl()+"')\"");
-					}
-					sb.append(">\n")
-					.append("<i class=\""+menu.getMenuIcon()+"\"></i>\n")
-					.append("<span class=\"title\">"+menu.getMenuName()+"</span>\n");
-					if(childNum > 0){
-						sb.append("<span class=\"arrow\"></span>\n");
-					}
-					sb.append("</a>\n");
-					if(childNum > 0){
-						sb.append(getChildren(menu.getChildren(),userMenus));
-					}
-					sb.append("</li>\n");
-				}
-			}
-		}
-		
-		return sb.toString();
-	}
-	
-	public static String getChildren(List<SysMenu> menuList,List<Long> userMenus){
-		StringBuilder sb = new StringBuilder();
-		
-		sb.append("<ul class=\"sub-menu\">\n");
-		
-		for(SysMenu menu : menuList){
-			
-			if(userMenus.contains(menu.getId())){
-				int childNum = menu.getChildren().size();
-				
-				sb.append("<li>\n")
-				.append("<a href=\"javascript:;\" ");
-				if(StringUtils.isNotEmpty(menu.getMenuUrl()) && childNum == 0){
-					sb.append("onclick=\"menu(this,'"+menu.getMenuUrl()+"')\"");
-				}
-				sb.append(">\n")
-				.append("<i class=\""+menu.getMenuIcon()+"\"></i>\n")
-				.append("<span class=\"title\">"+menu.getMenuName()+"</span>\n");
-				if(childNum > 0){
-					sb.append("<span class=\"arrow\"></span>\n");
-				}
-				sb.append("</a>\n");
-				if(childNum > 0){
-					sb.append(getChildren(menu.getChildren(),userMenus));
-				}
-				sb.append("</li>\n");
-			}
-		}
-		
-		sb.append("</ul>");
-		
-		return sb.toString();
-	}
+
+    public static String getMenu(List<SysMenu> menuList, List<Long> userMenus) {
+
+        StringBuilder sb = new StringBuilder();
+
+        if (menuList != null && menuList.size() > 0) {
+
+            for (SysMenu menu : menuList) {
+
+                if (userMenus.contains(menu.getId())) {
+                    int childNum = menu.getChildren().size();
+
+                    sb.append("<li>\n")
+                            .append("<a href=\"javascript:;\" ");
+                    if (StringUtils.isNotEmpty(menu.getMenuUrl()) && childNum == 0) {
+                        sb.append("onclick=\"menu(this,'"+menu.getMenuUrl()+"')\"");
+                    }
+                    sb.append(">\n")
+                            .append("<i class=\"" + menu.getMenuIcon() + "\"></i>\n")
+                            .append( menu.getMenuName() + "\n");
+                    if (childNum > 0) {
+                        sb.append("<span class=\"fa fa-chevron-down\"></span>\n");
+                    }
+                    sb.append("</a>\n");
+                    if (childNum > 0) {
+                        sb.append(getChildren(menu.getChildren(), userMenus));
+                    }
+                    sb.append("</li>\n");
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static String getChildren(List<SysMenu> menuList, List<Long> userMenus) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("<ul class=\"nav child_menu\">\n");
+
+        for (SysMenu menu : menuList) {
+
+            if (userMenus.contains(menu.getId())) {
+                int childNum = menu.getChildren().size();
+
+                sb.append("<li>\n")
+                        .append("<a href=\"javascript:;\"  ");
+                if (StringUtils.isNotEmpty(menu.getMenuUrl()) && childNum == 0) {
+                    sb.append("onclick=\"menu(this,'"+menu.getMenuUrl()+"')\"");
+                }
+                sb.append(">\n")
+                        .append("<i class=\"" + menu.getMenuIcon() + "\"></i>\n")
+                        .append( menu.getMenuName() + "\n");
+                if (childNum > 0) {
+                    sb.append("<span class=\"fa fa-chevron-down\"></span>\n");
+                }
+                sb.append("</a>\n");
+                if (childNum > 0) {
+                    sb.append(getChildren(menu.getChildren(), userMenus));
+                }
+                sb.append("</li>\n");
+            }
+        }
+
+        sb.append("</ul>");
+
+        return sb.toString();
+    }
 }
--- src/main/java/com/aibibang/app/controller/IndexController.java
+++ src/main/java/com/aibibang/app/controller/IndexController.java
@@ -29,7 +29,7 @@ public class IndexController extends BaseController {
 	@RequestMapping("/icon")
 	public String icon(HttpServletRequest request){
 		
-		return "system/icon/icon";
+		return "/icon/icon";
 	}
 	
 	@RequestMapping("/home")
